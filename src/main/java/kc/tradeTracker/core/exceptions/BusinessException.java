1eebfe570fdab01e3d39decb493dc276ea10f865
kaan cakir <124535986+kaanncakir@users.noreply.github.com>
2023-03-14 21:37:14 +0300

    Delete src directory

--- src/main/java/kc/tradeTracker/core/exceptions/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package kc.tradeTracker.core.exceptions;
-
-public class BusinessException extends RuntimeException
-{
-	 public BusinessException(String message)
-	 {
-	        super(message);
-	 }
-}
