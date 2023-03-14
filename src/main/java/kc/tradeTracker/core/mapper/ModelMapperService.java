8f7b6e7c7fd75cd3793142797c03eb39c17e18ad
kaan cakir <124535986+kaanncakir@users.noreply.github.com>
2023-03-14 21:35:39 +0300

    Add files via upload

---
+++ src/main/java/kc/tradeTracker/core/mapper/ModelMapperService.java
@@ -0,0 +1,10 @@
+package kc.tradeTracker.core.mapper;
+
+import org.modelmapper.ModelMapper;
+
+public interface ModelMapperService 
+{
+	 ModelMapper forResponse();
+
+	 ModelMapper forRequest();
+}
---
+++ src/main/java/kc/tradeTracker/dataAccess/TradeDao.java
@@ -0,0 +1,14 @@
+package kc.tradeTracker.dataAccess;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kc.tradeTracker.entities.Trade;
+
+@Repository
+public interface TradeDao extends JpaRepository<Trade,Integer>
+{
+	boolean existsByName(String name);
+}
